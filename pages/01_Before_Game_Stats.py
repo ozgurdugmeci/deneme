0d0b5c37f706e970747dc9bf5c312043dc33651e
Özgür Düğmeci <ozgur.dugmeci@gmail.com>
2024-10-04 10:47:31 +0300

    Update 01_Before_Game_Stats.py

--- pages/01_Before_Game_Stats.py
+++ pages/01_Before_Game_Stats.py
@@ -81,7 +81,7 @@ box1= df_clubs['name'].values.tolist()
 
 
 
-col1,col2 = st.columns([2.5,1])
+col1,col2 = st.columns([1,1])
 
 with col1:
  option = st.selectbox(
@@ -157,6 +157,82 @@ if option !=None:
  #df_results.loc[(df_results['home.name']!= option),'yeni2'] = 'at-> ' + df_results['home.name'] + ' ' +df_results['yeni']
  #df_results['yeni'] = df_results['yeni2']+ ' ' +df_results['away.score']
  box3= df_results['yeni'].values.tolist()
+
+#column2 -------------------------------------------------------------------------------------------------------------
+
+with col2:
+ option2 = st.selectbox(
+    'Select the team',
+    (box1), key='2',index=None)
+
+
+	
+if option !=None:
+
+ df_parca= df_clubs.loc[df_clubs['name']==option].copy()
+ df_parca = df_parca['url'].values.tolist()[0]
+ df_parca= df_parca.replace('roster','games')
+ df_parca=df_parca.replace('/','//')
  
- with col1:
-  option3 = st.selectbox ('Select the game', box3, key='3', index=None)
+ 
+ #2 
+ #sezon bilgisini al
+ #fikstür bilgisini al
+ 
+ 
+ linko_sezon= 'https://www.euroleaguebasketball.net'+ df_parca + '?season=2023-24'	
+ 
+ 
+ page = requests.get(linko_sezon)
+ 
+ soup = BeautifulSoup(page.content,"html.parser")
+ 
+ kk = soup.find(id="__NEXT_DATA__" )
+ 
+ 
+ for i in kk:
+  soup=i
+ 
+ site_json=json.loads(soup.string)
+ 
+ #dict_keys(['hero', 'results', 'seasons', 'clubCode', 'clubName', 'club'])
+ #dict_keys(['featuredGame', 'results', 'upcomingGames'])
+ 
+ df_sezon= pd.json_normalize(site_json['props']['pageProps']['seasons'])
+ 
+ box2=df_sezon['text'].values.tolist()
+ 
+ #with col1:
+ # option2 = st.selectbox(
+ #    'Select the season',
+ #    (box2), key='2')'''
+ 
+ 
+ linko_results= 'https://www.euroleaguebasketball.net'+ df_parca + '?season='+ str(box2[0])	
+ 
+ page = requests.get(linko_results)
+ 
+ soup = BeautifulSoup(page.content,"html.parser")
+ 
+ kk = soup.find(id="__NEXT_DATA__" )
+ 
+ for i in kk:
+  soup=i
+ 
+ site_json=json.loads(soup.string)
+ 	
+ 	
+ df_results= pd.json_normalize(site_json['props']['pageProps']['results']['results'])
+ df_results['home.score']= df_results['home.score'].astype(str)
+ df_results['away.score'] = df_results['away.score'].astype(str)
+ 
+ 
+ df_results['yeni'] = df_results['home.abbreviatedName'] + ' '+  df_results['home.score'] + ' - ' + df_results['away.abbreviatedName']+ ' ' +  df_results['away.score']  
+ 
+ #df_results['yeni'] = df_results['home.score']+ '-' +df_results['away.score']
+ #df_results.loc[(df_results['home.name']== option),'yeni2'] = 'vs-> ' +df_results['away.name'] +' ' + df_results['yeni']
+ #df_results.loc[(df_results['home.name']!= option),'yeni2'] = 'at-> ' + df_results['home.name'] + ' ' +df_results['yeni']
+ #df_results['yeni'] = df_results['yeni2']+ ' ' +df_results['away.score']
+ box3= df_results['yeni'].values.tolist()
+
+
