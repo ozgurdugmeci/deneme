510cf062dd998f03e4442164cf5aa5a908095b0e
Özgür Düğmeci <ozgur.dugmeci@gmail.com>
2024-10-15 13:52:40 +0300

    Update _home.py

--- _home.py
+++ _home.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.subheader('1-Team Comparison')
 #htp0='https://raw.githubusercontent.com/ozgurdugmeci/easy-app/main/media/model4.jpg'
 
-img1 ='https://raw.githubusercontent.com/ozgurdugmeci/deneme/main/media/comparison.jpg'
+img1 ='https://raw.githubusercontent.com/ozgurdugmeci/deneme/main/comparison.jpg'
 st.image(img1,width=600)
 
 st.subheader('2-Finished Game Analysis')
