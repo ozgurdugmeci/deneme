a5135723c62f9d1799487d9ef988fd7d4ef00f0c
Özgür Düğmeci <ozgur.dugmeci@gmail.com>
2024-10-15 15:11:26 +0300

    Update 01_Team_Comparision.py

--- pages/01_Team_Comparision.py
+++ pages/01_Team_Comparision.py
@@ -550,4 +550,21 @@ if option != None and optionto != None :
   config = {'staticPlot': True}
   st.plotly_chart(fig,config=config)
 
-
+"""
+<!-- Default Statcounter code for Euro_stats_compare
+https://euroleaguestats.streamlit.app/ -->
+<script type="text/javascript">
+var sc_project=13046770; 
+var sc_invisible=1; 
+var sc_security="ee0f4109"; 
+</script>
+<script type="text/javascript"
+src="https://www.statcounter.com/counter/counter.js"
+async></script>
+<noscript><div class="statcounter"><a title="Web Analytics"
+href="https://statcounter.com/" target="_blank"><img
+class="statcounter"
+src="https://c.statcounter.com/13046770/0/ee0f4109/1/"
+alt="Web Analytics"
+referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
+<!-- End of Statcounter Code -->"""
