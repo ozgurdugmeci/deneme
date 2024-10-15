cd068b563e9b41d6211cf1eea0e90f19919aaaa7
Özgür Düğmeci <ozgur.dugmeci@gmail.com>
2024-10-15 13:18:44 +0300

    Update 01_Team_Comparision.py

--- pages/01_Team_Comparision.py
+++ pages/01_Team_Comparision.py
@@ -8,7 +8,7 @@ import plotly.express as px
 import pandas as pd
 
 
-st.set_page_config(page_title='Comparison', page_icon="ℹ️", layout="wide",initial_sidebar_state ='collapsed')
+st.set_page_config(page_title='Comparison', page_icon="🏀", layout="wide",initial_sidebar_state ='collapsed')
 
 
 
