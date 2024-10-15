72ca70df7f7e48ac7908887eb3584530f2e920bc
Özgür Düğmeci <ozgur.dugmeci@gmail.com>
2024-10-15 14:24:23 +0300

    Update _home.py

--- _home.py
+++ _home.py
@@ -3,6 +3,7 @@ import streamlit as st
 
 st.subheader('1-Team Comparison')
 #htp0='https://raw.githubusercontent.com/ozgurdugmeci/easy-app/main/media/model4.jpg'
+metin='sdsdssd <br> dfdfdfd'
 
 img1 ='https://raw.githubusercontent.com/ozgurdugmeci/deneme/main/comparison.jpg'
 st.image(img1,width=500)
