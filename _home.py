1355c279d31d8ef57b021489a6f3b92cb1625e48
Özgür Düğmeci <ozgur.dugmeci@gmail.com>
2024-10-15 13:53:40 +0300

    Update _home.py

--- _home.py
+++ _home.py
@@ -5,7 +5,7 @@ st.subheader('1-Team Comparison')
 #htp0='https://raw.githubusercontent.com/ozgurdugmeci/easy-app/main/media/model4.jpg'
 
 img1 ='https://raw.githubusercontent.com/ozgurdugmeci/deneme/main/comparison.jpg'
-st.image(img1,width=800)
+st.image(img1,width=950)
 
 st.subheader('2-Finished Game Analysis')
 st.subheader('3-Player Stats')
