c119dbb328f68d673ab460eea427e7ab03d5a0b1
Özgür Düğmeci <ozgur.dugmeci@gmail.com>
2024-10-15 14:18:55 +0300

    Update _home.py

--- _home.py
+++ _home.py
@@ -7,7 +7,7 @@ st.subheader('1-Team Comparison')
 img1 ='https://raw.githubusercontent.com/ozgurdugmeci/deneme/main/comparison.jpg'
 st.image(img1,width=500)
 
-st.subheader('2-Finished Game Analysis')
+st.subheader('2-Finished Game Analysis & Productive5')
 img2 ='https://raw.githubusercontent.com/ozgurdugmeci/deneme/main/game_analysis1.jpg'
 st.image(img2,width=500)
 img3 ='https://raw.githubusercontent.com/ozgurdugmeci/deneme/main/game_analysis2.jpg'
